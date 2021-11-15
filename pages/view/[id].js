d72e0de59066acfad02f35304f2fe2ff613fffad
deokhyun-dv <dev.deokhyun@gmail.com>
2021-11-15 22:50:56 +0900

    SSR 상세페이지 구현

--- pages/view/[id].js
+++ pages/view/[id].js
@@ -1,5 +1,39 @@
-export default function dynamicFUnc() {
-    return (
-        <div> 다이나믹 라우트</div>
-    )
+import React, { useEffect, useState } from "react";
+import Head from "next/head";
+import Axios from "axios";
+import { useRouter } from "next/router";
+import Item from "../../src/component/Item";
+import Loading from "../../src/component/Loader";
+
+const Post = ({ item }) => {
+  console.log(item);
+  return (
+    <>
+      {item && (
+        <>
+          <Head>
+            <title>{item.name}</title>
+            <meta name="description" content={item.description}></meta>
+          </Head>
+          <Item item={item} />
+        </>
+      )}
+    </>
+  );
+};
+
+export default Post;
+
+export async function getServerSideProps(context) {
+  // console.log(context);
+  const id = context.params.id;
+  const apiUrl = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
+  const res = await Axios.get(apiUrl);
+  const data = res.data;
+
+  return {
+    props: {
+      item: data,
+    },
+  };
 }
--- src/component/Top.js
+++ src/component/Top.js
@@ -1,4 +1,5 @@
 import { Header, Menu, Input } from "semantic-ui-react";
+import Link from "next/link";
 import Gnb from "./Gnb";
 
 export default function Top() {
@@ -6,14 +7,17 @@ export default function Top() {
   return (
     <>
       <div>
-        <div style={{ display: "flex" }}>
-          <div style={{ flex: "100px 0 0" }}>
-            <img
-              src="/images/dune.png"
-              alt="logo"
-              style={{ display: "block", width: 80 }}
-            />
-          </div>
+        <div style={{ display: "flex", paddingTop: 30 }}>
+          <Link href={"/"}>
+            <div style={{ flex: "100px 0 0" }}>
+              <img
+                src="/images/dune.png"
+                alt="logo"
+                style={{ display: "block", width: 80, cursor: "pointer" }}
+              />
+            </div>
+          </Link>
+
           <Header as="hi"> Next JS</Header>
         </div>
 
